556e6569e77bcce944cf97297d506b5a1def9d00
Rusty <rusty3925@gmail.com>
2025-04-08 12:56:20 -0700

    adding in a hello world from wikipedia

--- whitespace_test.py
+++ whitespace_test.py
@@ -1,10 +1,16 @@
+import re
 import json
 
 import whitespace
 
 from lark import Transformer
 
-PROGRAM = "\n  \t \n  \t \n"
+PROGRAM_CHAR = re.compile(r'[ \t\n]')
+
+def minify(program: str):
+    return ''.join(m.group(0) for m in PROGRAM_CHAR.finditer(program))
+
+PROGRAM = minify(open('hello.ws').read())
 
 if __name__ == '__main__':
     output = whitespace.parser.parse(PROGRAM)
