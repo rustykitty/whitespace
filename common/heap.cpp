a53a71b119a7830d6b8f78ca4e126b73ea103be7
Rusty <rusty3925@gmail.com>
2025-07-01 12:10:28 -0700

    C++ heap implementation

---
+++ common/heap.cpp
@@ -0,0 +1,19 @@
+#include "heap.h"
+
+#include <unordered_map>
+
+static std::unordered_map<ws_int, ws_int> heap;
+
+extern "C" {
+    ws_int heap_load(ws_int addr) {
+        auto it = heap.find(addr);
+        if (it != heap.end()) {
+            return it->second;
+        }
+        return 0;
+    }
+
+    void heap_store(ws_int addr, ws_int val) {
+        heap[addr] = val;
+    }
+}
---
+++ common/heap.h
@@ -0,0 +1,20 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "whitespace.h"
+
+#include <stdbool.h>
+#include <sys/types.h>
+
+ws_int heap_load(ws_int);
+void heap_store(ws_int, ws_int);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HEAP_H
