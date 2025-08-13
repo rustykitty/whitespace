b92ff913323781f4be28a8711684f64289d4dfa4
Rusty <rusty3925@gmail.com>
2025-08-13 14:34:22 -0700

    remove tests because I never use them

--- python/tests/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestWhitespaceParser(unittest.TestCase):
-    def test_import_parser(self):
-        from whitespace_module import exec_tree, parse_and_exec
-    
-    def test_parse_and_exec(self):
-        from whitespace_module import parse_and_exec
-        parse_and_exec("\n\n\n") # only one END instruction
