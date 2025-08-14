338e005a46b5cf4061f996580e8f0aa1afabb512
Rusty <rusty3925@gmail.com>
2025-08-14 14:05:55 -0700

    fix assembler errors

--- python/whitespace_assemble_and_run.py
+++ python/whitespace_assemble_and_run.py
@@ -8,6 +8,6 @@ def run(data, filename=None):
     try:
         output = wsasm.parse(data)
         transformed_output = wsasm.TRANSFORMER.transform(output)
-        ws.interpret_c_wrapper(transformed_output)
+        ws.run(transformed_output)
     except lark.exceptions.LarkError as e:
         raise
